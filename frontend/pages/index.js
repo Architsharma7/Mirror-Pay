604055201bf2db03376f162f1efba42f1062162c
Archit <archit@Archits-MacBook-Air.local>
2022-09-11 15:01:50 +0530

    updated

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,11 +2,23 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
-    <div>
+    <div
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
       <ConnectButton
         accountStatus={{
           smallScreen: "avatar",
           largeScreen: "full",
+        }}
+        showBalance={{
+          smallScreen: false,
+          largeScreen: true,
         }}
       />
     </div>
