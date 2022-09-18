7152526d5be5b09c0b4c43f93623c77b79231f52
atj3097 <adamthomasjackson3@gmail.com>
2022-09-17 21:04:37 -0400

    Create company_treasury.sol

---
+++ hardhat-0x/contracts/company_treasury.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+contract Treasury is PaymentSplitter { 
+    // add emitted events 
+   mapping(address => string) private _eors;
+
+    constructor() {
+
+    }
+
+    function treasuryBalanceCheck() { 
+
+    }
+
+    function payout() {
+        
+    }
+
+    function deployNewEOR() { 
+
+    }
+
+}
