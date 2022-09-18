f7543b7e2af6c1a61adc3cd97986cf6080ef608a
atj3097 <adamthomasjackson3@gmail.com>
2022-09-17 22:14:10 -0400

    test escrow smart contract

--- hardhat-0x/contracts/company_treasury.sol
+++ hardhat-0x/contracts/company_treasury.sol
@@ -1,25 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
-contract Treasury is PaymentSplitter { 
-    // add emitted events 
-   mapping(address => string) private _eors;
+pragma solidity >=0.7.0 <0.9.0;
 
-    constructor() {
+/**
+ * @title EscrowSol
+ * @dev Manages fund transfers between two parties
+ */
+contract EscrowSol {
+    // Wallet address of the payer
+    address public funder;
+    // Wallet address of the intended beneficiary
+    address public beneficiary;
 
+    /// Lockup a certain crypto value.
+    /// @param counterpart the address of the intended beneficiary
+    /// @dev lockup crypto for the counterpart
+    function fund(address counterpart) public payable {
+        beneficiary = counterpart;
+        funder = msg.sender;
     }
 
-    function treasuryBalanceCheck() { 
-
+    /// Release all locked funds.
+    /// @dev The deal is done, let only the payer release fund.
+    function release() public payable {
+        if (msg.sender==funder){
+            // Transfer all the funds to the beneficiary
+            payable(beneficiary).transfer(address(this).balance);
+        }
     }
 
-    function payout() {
-        
+    /// Return the locked value.
+    /// @dev anyone should be able to see the actually locked crpto value .
+    /// @return the crypto value
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
-
-    function deployNewEOR() { 
-
-    }
-
 }
