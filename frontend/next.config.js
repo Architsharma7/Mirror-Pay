479fdea778658fb7af171d0792daf4151f5feeb8
atj3097 <adamthomasjackson3@gmail.com>
2022-09-22 16:24:30 -0400

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-export const chainId = 80001
-export const url =
-  "https://polygon-mumbai.g.alchemy.com/v2/uny4ZM2m1-KrNvRxuflNcJ3d0psTynDL";
-export const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
 module.exports = nextConfig
