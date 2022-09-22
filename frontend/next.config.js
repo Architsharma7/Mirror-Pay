a1ebfb94e775b7111bb295004fd1d5cd0094118a
atj3097 <adamthomasjackson3@gmail.com>
2022-09-22 11:39:35 -0400

    Revert "Merge pull request #3 from Architsharma7/ajackson/super-fluid-implementation"
    This reverts commit a8797687ff7bd486ca4a0d47a40b8b9320920767, reversing
    changes made to f274de720e5360e9f633a5da5583864584449e33.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,10 +4,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-export const Framework = require("@superfluid-finance/sdk-core");
-export const ethers = require("ethers");
-
-export const url =
-  "https://polygon-mumbai.g.alchemy.com/v2/uny4ZM2m1-KrNvRxuflNcJ3d0psTynDL";
-export const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
 module.exports = nextConfig
